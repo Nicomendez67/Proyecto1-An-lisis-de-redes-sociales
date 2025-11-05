c4eb3593782e0adfbee9924c1d80bc0d8c9d712c
Nicomendez67 <n.mendez@correo.unimet.edu.ve>
2025-11-04 22:51:54 -0400

    Grafo.java

--- Proyecto1/src/proyecto1/Grafo.java
+++ Proyecto1/src/proyecto1/Grafo.java
@@ -9,5 +9,21 @@ package proyecto1;
  * @author JAVIER MENDEZ
  */
 public class Grafo {
+
+    private final int numVertices;
+    private final boolean esDirigido;
+    private final Lista[] adyacentes;
+
+    public Grafo(int numVertices, boolean esDirigido) {
+        if (numVertices <= 0) {
+            throw new IllegalArgumentException("El número de vértices debe ser positivo.");
+        }
+        this.numVertices = numVertices;
+        this.esDirigido = esDirigido;
+        this.adyacentes = new Lista[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adyacentes[i] = new Lista();
+        }
     
 }
+
